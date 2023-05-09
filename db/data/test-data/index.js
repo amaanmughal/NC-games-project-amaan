07b70df2443a7cb4b41453eab1b92c3975a6e74e
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-05-09 13:33:29 +0100

    task 2 repo setup

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,4 +1,4 @@
-exports.categoryData = require('./categories.js');
-exports.commentData = require('./comments.js');
-exports.reviewData = require('./reviews.js');
-exports.userData = require('./users.js');
+exports.categoryData = require("./categories.js");
+exports.commentData = require("./comments.js");
+exports.reviewData = require("./reviews.js");
+exports.userData = require("./users.js");
